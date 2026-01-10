45f55ed4a174132f7a2da33639e7ec0c8f42b78d
Emilio P Egido <eaperez@me.com>
2026-01-10 19:39:11 +0100

    Update deployment target and simplify AppMain

--- DockProgress-test/App.swift
+++ DockProgress-test/App.swift
@@ -6,19 +6,11 @@ struct AppMain: App {
     private let appState = AppState()
 
     var body: some Scene {
-        if #available(macOS 13.0, *) {
             WindowGroup {
                 ContentView()
                     .navigationTitle("")
                     .environmentObject(appState)
             }
             .windowResizability(.contentSize)
-        } else {
-            WindowGroup {
-                ContentView()
-                    .navigationTitle("")
-                    .environmentObject(appState)
-            }
         }
     }
-}
